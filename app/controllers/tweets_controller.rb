8a31be4f0525fabc37c440f71eeca701d57417d7
Luka Kukina <lukscbt@gmail.com>
2020-03-29 10:08:35 +0200

    devise gem

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -29,8 +29,8 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
-        format.json { render :show, status: :created, location: @tweet }
+        format.html { redirect_to root_path, notice: 'Tweet was successfully created.' }
+        format.json { redirect_to root_path, status: :created, location: @tweet }
       else
         format.html { render :new }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
